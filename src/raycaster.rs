cc488120af3d729b4d21266d09c2e92d81e887b5
Patrick José Pereira <patrickelectric@gmail.com>
2019-09-01 14:25:18 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/raycaster.rs
+++ src/raycaster.rs
@@ -1,14 +1,50 @@
 extern crate piston_window;
 
 use std::vec::Vec;
-use std::convert::From;
+
+pub struct Player {
+    pos: (f64, f64),
+    fov: f64,
+    projection_plane_distance: f64,
+}
+
+impl Default for Player {
+    fn default() -> Player {
+        return Player {
+            pos: (350.0, 350.0),
+            fov: 135.0,
+            projection_plane_distance: 10.0,
+        };
+    }
+}
 
 pub struct Environment {
-    map: Vec<Vec<bool>>,
-    scale: u32,
+    map: Vec<u64>,
+    scale: f64,
+    player: Player,
+}
+
+#[rustfmt::skip::macros(vec)]
+impl Default for Environment {
+    fn default() -> Environment {
+        return Environment {
+            map: vec![
+                1, 1, 1, 1, 1, 1, 1,
+                1, 0, 0, 0, 0, 0, 1,
+                1, 0, 0, 0, 0, 0, 1,
+                1, 0, 0, 0, 0, 0, 1,
+                1, 0, 0, 0, 0, 0, 1,
+                1, 0, 0, 0, 0, 0, 1,
+                1, 1, 1, 1, 1, 1, 1,
+            ],
+            scale: 100.0,
+            player: Player::default(),
+        };
+    }
 }
 
 /*
+use std::convert::From;
 impl From<(usize, usize)> for (u32, u32) {
     fn from(value: (usize, usize)) -> (u32, u32) {
         (value.0 as u32, value.1 as u32)
@@ -17,27 +53,35 @@ impl From<(usize, usize)> for (u32, u32) {
 */
 
 impl Environment {
-    pub fn draw(&self, texture: piston_window::G2dTexture, pos: (u32, u32)) {
-        let fov: f64 = 135.0*std::f64::consts::PI/180.0;
+    pub fn draw(&self, texture: &mut piston_window::G2dTexture) {
+        // Fov in radians
+        let fov_rad: f64 = self.player.fov * std::f64::consts::PI / 180.0;
         // Only rectangles for now
-        let cell_size = self.scale/self.map.len() as u32;
+        let cell_size = self.scale / self.size() as f64;
 
-        // Horizontal scan
+        // Total number of steps around fov angle
         for n in -5..=5 {
-            // Distance scan
+            let n_float = n as f64;
+            // Total number of steps until hit or end of map
             for d in 1..10 {
-                let new_pos = ((n as f64*fov/10.0).cos()*d as f64, (n as f64*fov/10.0).sin()*d as f64);
-                //texture.surface.0;
+                let d_float = d as f64;
+
+                //let new_pos = ((n_float*fov/10.0).cos()*d_float, (n_float*fov/10.0).sin()*d_float);
+                //println!("{:#?}", new_pos);
             }
         }
     }
 
-    pub fn is_position_inside(&self, pos: (u32, u32)) -> bool {
-        let size = self.size();
-        return pos < (size.0 as u32*self.scale, size.1 as u32*self.scale) && pos > (0, 0);
+    pub fn set_map(&mut self, map: Vec<u64>) {
+        self.map = map.clone();
     }
 
-    pub fn size(&self) -> (usize, usize) {
-        return (self.map.len(), self.map[0].len());
+    pub fn is_position_inside(&self, pos: (u64, u64)) -> bool {
+        let size = self.size();
+        return pos > (0, 0) && pos < (size, size);
+    }
+
+    pub fn size(&self) -> u64 {
+        return (self.map.len() as f64).sqrt() as u64;
     }
 }
